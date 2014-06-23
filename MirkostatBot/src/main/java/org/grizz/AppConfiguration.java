55cba84013a5e74901d705c30173f7405bb2e9f1
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@f5261dea-868e-766a-caa3-ab70510df6f8>
2014-06-23 06:38:05 +0000

    Logowanie do wykop przy starcie aplikacji

--- MirkostatBot/src/main/java/org/grizz/AppConfiguration.java
+++ MirkostatBot/src/main/java/org/grizz/AppConfiguration.java
@@ -1,37 +1,31 @@
 package org.grizz;
 
-import org.grizz.printer.ApplicationTagPrinter;
-import org.grizz.printer.BestVotedUsersPrinter;
-import org.grizz.printer.MostActiveUserPrinter;
-import org.grizz.printer.SeparatorPrinter;
-import org.grizz.printer.StatPrinter;
-import org.grizz.printer.StatSummaryPrinter;
-import org.grizz.printer.TagStatsPrinter;
-import org.grizz.printer.UserCharactersStatPrinter;
-import org.grizz.printer.UserEntriesStatPrinter;
+import com.google.gson.Gson;
+import org.apache.commons.lang3.Validate;
+import org.grizz.model.UserKeyModel;
+import org.grizz.printer.*;
 import org.grizz.service.EntryProvider;
+import org.grizz.service.HTTPRequestService;
 import org.grizz.service.URLBuilder;
 import org.grizz.service.URLEncoder;
+import org.grizz.service.post.WykopUrlSigner;
 import org.grizz.statistics.StatCollectorPool;
 import org.grizz.statistics.StatCollectorPoolFactory;
-import org.grizz.statistics.collector.EntryCounter;
-import org.grizz.statistics.collector.EntryLengthCollector;
-import org.grizz.statistics.collector.MostActiveUser;
-import org.grizz.statistics.collector.StatCollector;
-import org.grizz.statistics.collector.TagCounter;
-import org.grizz.statistics.collector.TagRateCounter;
-import org.grizz.statistics.collector.TotalVoteCollector;
-import org.grizz.statistics.collector.UserCounter;
-import org.grizz.statistics.collector.UserEntryCounter;
-import org.grizz.statistics.collector.UserTypedCharactersCounter;
+import org.grizz.statistics.collector.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class AppConfiguration {
-	private static final String APP_KEY = "QRyIcO0ZUu";
-	private static final String SECRET_KEY = "E3NWFSkUqN";
+	private static final String APP_KEY = "TPJl24Jc5J";
+	private static final String SECRET_KEY = "mo4JG9Ed4E";
+	private static final String ACCOUNT_KEY = "Ypbaxw0FUUtS4swAvxpI";
+//	private static final long COLLECTION_PERIOD = 400000; //test
 	private static final long COLLECTION_PERIOD = 86400000; //doba
 //	private static final long COLLECTION_PERIOD = 3600000l; //1 godzina
 	private static final int AMOUNT_OF_TAG_STATS = 10;
@@ -70,11 +64,38 @@ public class AppConfiguration {
 	@Autowired
 	private MostActiveUserPrinter mostActiveUserPrinter;
 
+    @Autowired
+    private HTTPRequestService requestService;
+    @Autowired
+    private WykopUrlSigner signer;
+
 	@Bean
 	public TagStatsPrinter tagStatsPrinter() {
 		return new TagStatsPrinter(AMOUNT_OF_TAG_STATS);
 	}
-	
+
+    @Bean(name = "userKey")
+    public String userKey() {
+        URLBuilder urlBuilder = urlBuilder();
+        Map<String, String> postContent = new HashMap<>();
+        String url = urlBuilder.getWykopLoginURL();
+
+        postContent.put("accountkey", ACCOUNT_KEY);
+
+        String json = requestService.sendPost(url, postContent, Collections.EMPTY_MAP);
+
+        Gson gson = new Gson();
+        UserKeyModel userKeyModel = gson.fromJson(json, UserKeyModel.class);
+        Validate.notNull(userKeyModel.getUserkey());
+
+        return userKeyModel.getUserkey();
+    }
+
+    @Bean
+    public WykopUrlSigner wykopUrlSigner() {
+        return new WykopUrlSigner(SECRET_KEY);
+    }
+
 	@Bean
 	public URLBuilder urlBuilder() {
 		return new URLBuilder(APP_KEY);
