22c988c20d8bb064c64236bff64d88108f14f81a
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@f5261dea-868e-766a-caa3-ab70510df6f8>
2014-06-23 06:44:19 +0000

    Usuniecie starych, niepotrzebnych klas

--- MirkostatBot/src/main/java/org/grizz/utils/Emergency.java
+++
@@ -1,9 +0,0 @@
-package org.grizz.utils;
-
-public class Emergency {
-	public static void checkPrecondition(boolean condition, String message) {
-		if(!condition) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-}
