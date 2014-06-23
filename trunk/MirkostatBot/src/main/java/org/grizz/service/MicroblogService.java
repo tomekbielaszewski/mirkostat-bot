e07c42e8868db6944ba575cfad1ec0b035e6c3e8
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@f5261dea-868e-766a-caa3-ab70510df6f8>
2014-06-23 06:41:30 +0000

    Implementacja nowego pobierania wpisow - teraz mozliwe pobieranie o dowolnej glebokosci

--- trunk/MirkostatBot/src/main/java/org/grizz/service/MicroblogService.java
+++ trunk/MirkostatBot/src/main/java/org/grizz/service/MicroblogService.java
@@ -1,31 +1,60 @@
 package org.grizz.service;
 
 import org.grizz.model.Entry;
+import org.grizz.service.post.WykopUrlSigner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.google.gson.Gson;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MicroblogService {
-//	private URLEncoder encoder;
-	
 	@Autowired
 	private URLBuilder builder;
-	
 	@Autowired
 	private HTTPRequestService requestService;
-	
-	public Entry[] index() {
-		return index(0);
+    @Autowired
+    private EntryFactory entryFactory;
+    @Autowired
+    private WykopUrlSigner signer;
+
+    public Entry[] index() {
+		return index(1);
 	}
 	
 	public Entry[] index(int page) {
+        System.out.print("Page: " + page);
+
 		String json = requestService.sendGet(builder.getLatestMicroblogEntriesURL(page));
-		Gson gson = new Gson();
-		
-		Entry[] entries = gson.fromJson(json, Entry[].class);
-		
-		return entries;
+		Entry[] entries = entryFactory.getEntries(json);
+
+        System.out.println(" | Last entry date: " + new SimpleDateFormat("d k:m:s").format(entries[entries.length - 1].getDateAdded()));
+
+        return entries;
 	}
+
+    public Entry[] next(Long entryId) {
+        System.out.print("First id: " + entryId);
+        String json = requestService.sendGet(builder.getMicroblogEntriesAfterURL(entryId));
+        Entry[] entries = entryFactory.getEntries(json);
+        System.out.print(" | Last id: " + entries[entries.length - 1].getId());
+        System.out.println(" | Last entry date: " + new SimpleDateFormat("dd kk:mm:ss").format(entries[entries.length - 1].getDateAdded()));
+        return entries;
+    }
+
+    public String add(String entryBody) {
+        Map<String, String> postContent = new HashMap<>();
+        Map<String, String> headers = new HashMap<>();
+        String url = builder.getMicroblogAddEntryURL();
+
+        postContent.put("body", entryBody);
+
+        return requestService.sendPost(url, postContent, headers);
+    }
+
+    public Entry[] next(int entryId) {
+        return next(Long.valueOf(entryId));
+    }
 }
