8afc7a872b62e72d551e039386061917e19cc23d
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@f5261dea-868e-766a-caa3-ab70510df6f8>
2014-06-23 06:42:06 +0000

    Przejscie na fluen API z pakietu Apache Http Components

--- MirkostatBot/src/main/java/org/grizz/service/HTTPRequestService.java
+++ MirkostatBot/src/main/java/org/grizz/service/HTTPRequestService.java
@@ -1,40 +1,69 @@
 package org.grizz.service;
 
+import org.apache.http.Header;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicHeader;
+import org.grizz.service.post.WykopUrlSigner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.springframework.stereotype.Service;
 
 @Service
 public class HTTPRequestService {
+    @Autowired
+    private WykopUrlSigner signer;
 	
 	public String sendGet(String url) {
 		String responseContent = "";
-		
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(url);
-		
-		try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-			responseContent = EntityUtils.toString(response.getEntity());
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+        Request getRequest = Request.Get(url);
+
+        getRequest.addHeader(new BasicHeader("apisign", signer.sign(url, Collections.EMPTY_MAP)));
+
+        try {
+            responseContent = getRequest.execute().returnContent().asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 		return responseContent;
 	}
 	
-//	public String sendPost(String url, Map<String, String> postContent) {
-//		CloseableHttpClient httpClient = HttpClients.createDefault();
-//		HttpPost httpPost = new HttpPost(url);
-//		httpPost.setConfig(config);
-//	}
+	public String sendPost(String url, Map<String, String> postContent, Map<String, String> headers) {
+        String responseContent = "";
+        Form form = Form.form();
+        List<Header> headersList = new ArrayList<>();
+
+        for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+            headersList.add(new BasicHeader(headerEntry.getKey(), headerEntry.getValue()));
+        }
+
+        for (Map.Entry<String, String> entry : postContent.entrySet()) {
+            form.add(entry.getKey(), entry.getValue());
+        }
+
+        Request postRequest = Request.Post(url);
+
+        postRequest.addHeader(new BasicHeader("apisign", signer.sign(url, postContent)));
+
+        for (Header header : headersList) {
+            postRequest.addHeader(header);
+        }
+
+        try {
+            responseContent = postRequest
+                    .bodyForm(form.build())
+                    .execute().returnContent().asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseContent;
+	}
 }
