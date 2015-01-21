0fc922db957367776cc9b4231bf63d04f4e53358
tomekbielaszewski@gmail.com <tomekbielaszewski@gmail.com@f5261dea-868e-766a-caa3-ab70510df6f8>
2015-01-21 19:18:45 +0000

    Changelist:               - Usunieto bug licznika komentarzy               - Wersja 1.5.5

--- trunk/MirkostatBot/src/main/java/org/grizz/statistics/collector/EntryCommentCounter.java
+++ trunk/MirkostatBot/src/main/java/org/grizz/statistics/collector/EntryCommentCounter.java
@@ -1,21 +1,25 @@
 package org.grizz.statistics.collector;
 
 import org.grizz.model.Entry;
+import org.grizz.model.EntryComment;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Grizz on 2014-07-04.
  */
 @Service
 public class EntryCommentCounter extends AbstractStatCounter {
-    private int numberOfComments;
+    private Set<EntryComment> comments = new HashSet<>();
 
     @Override
     public void collect(Entry entry) {
-        numberOfComments += entry.getComments().size();
+        comments.addAll(entry.getComments());
     }
 
     public int getNumberOfComments() {
-        return numberOfComments;
+        return comments.size();
     }
 }
