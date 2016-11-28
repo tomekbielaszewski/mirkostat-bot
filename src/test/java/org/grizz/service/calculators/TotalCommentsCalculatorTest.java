807c7461c0019e7c8ba26f40d6337857ea6b382e
Grizwold <tomekbielaszewski@gmail.com>
2016-11-28 19:14:24 +0100

    Total comments counter

---
+++ src/test/java/org/grizz/service/calculators/TotalCommentsCalculatorTest.java
@@ -0,0 +1,28 @@
+package org.grizz.service.calculators;
+
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TotalCommentsCalculatorTest {
+    @Test
+    public void consume() throws Exception {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void getName() throws Exception {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void getValue() throws Exception {
+        throw new NotImplementedException();
+    }
+
+}
