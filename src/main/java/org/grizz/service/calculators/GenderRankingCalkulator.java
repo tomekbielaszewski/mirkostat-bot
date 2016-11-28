58f1fead40dda6d8fdda35c1054d305dd730571a
Grizwold <tomekbielaszewski@gmail.com>
2016-11-28 19:21:17 +0100

    gender ranking

---
+++ src/main/java/org/grizz/service/calculators/GenderRankingCalkulator.java
@@ -0,0 +1,38 @@
+package org.grizz.service.calculators;
+
+import org.grizz.model.Embed;
+import org.grizz.model.Entry;
+import org.grizz.service.calculators.structures.Ranking;
+import org.grizz.service.calculators.structures.SummingRanking;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Component
+public class GenderRankingCalkulator implements StatisticsCalculator {
+    public static final String NAME = "genderRanking";
+    private Ranking ranking = new SummingRanking();
+
+    @Override
+    public void consume(Set<Entry> entries) {
+        entries.forEach(e -> {
+            ranking.add(e.getAuthorSex());
+            e.getVoters().forEach(vote -> ranking.add(vote.getAuthorSex()));
+            e.getComments().forEach(c -> {
+                ranking.add(c.getAuthorSex());
+                c.getVoters().forEach(cVote -> ranking.add(cVote.getAuthorSex()));
+            });
+        });
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public Object getValue() {
+        return ranking.asList();
+    }
+}
