67f95b58d5696a3451775ad91287c1afc7c20e98
Grizwold <tomekbielaszewski@gmail.com>
2016-11-28 19:29:30 +0100

    image ranking

--- src/main/java/org/grizz/model/Embed.java
+++ src/main/java/org/grizz/model/Embed.java
@@ -2,6 +2,7 @@ package org.grizz.model;
 
 import lombok.Data;
 import lombok.experimental.Builder;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 @Builder
@@ -9,4 +10,11 @@ public class Embed {
     private boolean plus18;
     private String type;
     private String url;
+
+    public EmbedType getType() {
+        if(StringUtils.isEmpty(type)) {
+            return null;
+        }
+        return EmbedType.valueOf(type.toUpperCase());
+    }
 }
---
+++ src/main/java/org/grizz/service/calculators/ImageRankingCalculator.java
@@ -0,0 +1,45 @@
+package org.grizz.service.calculators;
+
+import org.grizz.model.Embed;
+import org.grizz.model.EmbedType;
+import org.grizz.model.Entry;
+import org.grizz.model.EntryComment;
+import org.grizz.service.calculators.structures.Ranking;
+import org.grizz.service.calculators.structures.SimpleRanking;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class ImageRankingCalculator implements StatisticsCalculator {
+    public static final String NAME = "bestImages";
+    private Ranking ranking = new SimpleRanking();
+
+    @Override
+    public void consume(Set<Entry> entries) {
+        entries.forEach(e -> {
+            if(isImagePresent(e.getEmbed())) {
+                ranking.add(e, e.getVotes());
+            }
+            e.getComments().forEach(c -> {
+                if(isImagePresent(c.getEmbed())) {
+                    ranking.add(c, c.getVotes());
+                }
+            });
+        });
+    }
+
+    private boolean isImagePresent(Embed embed) {
+        return EmbedType.IMAGE.equals(embed.getType());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public Object getValue() {
+        return ranking.asList();
+    }
+}
